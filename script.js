8abee880c8ac7b7bbb76384be0b14ef6aff07473
user <user@userui-MacBookPro.local>
2023-09-17 15:53:05 +0900

    2nd

---
+++ script.js
@@ -0,0 +1,28 @@
+// Sample data
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var svgWidth = 500, svgHeight = 300, barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
+
+var svg = d3.select('svg');
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgHeight]);
+
+// Create bars
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+        return svgHeight - yScale(d);
+    })
+    .attr("height", function(d) {
+        return yScale(d);
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function (d, i) {
+        var translate = [barWidth * i, 0];
+        return "translate(" + translate + ")";
+    });
